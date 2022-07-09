c9f08cf77c40bafc0e16b9d77e047de340283809
gabriel <gabrielmouraguimaraes@gmail.com>
2022-07-09 18:40:06 -0300

    criando o card

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,11 +1,13 @@
-import "../assets/profile.jpg"
+import img from '../assets/profile.jpg'
 
 const styles = {
     root: {
         margin: '5vw'
     },
+    
     avatar: {
         width: 100,
+        height: 100,
         borderRadius: '50%',
         padding: 5,
         boxShadow: '0px 5px 25px 0px rgba(0,0,0,0.5)'
@@ -16,7 +18,7 @@ export default function Avatar() {
     return (
         <div style={styles.root}>
             <img
-                src="./src/assets/profile.jpg"
+                src={img}
                 alt="Avatar"
                 style={styles.avatar}
             />
--- src/components/item-user.tsx
+++ src/components/item-user.tsx
@@ -1,29 +1,45 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 import Avatar from './avatar'
-
-
-export interface IUserItemProps {
-    nome: string;
-    profissao: string;
-}
+import img from './avatar'
+import { userInfo } from 'os'
 
 interface UserItemProps {
-    nome: string;
-    profissao: string;
+    user: {
+        nome: string,
+        profissao: string,
+        avatarUrl: any
+    }
+}
+
+const styles = {
+  nome: {
+
+  },  
+
+  profissao: {
+
+  }
 }
 
 export default function ItemUser(props: UserItemProps) {
 
     const [state, setState] = React.useState<any>(
-
-
-
+        props.user = {
+            nome: '',
+            profissao: '',
+            avatarUrl: {img}
+        }
     );
 
+    
     return (
-
-        <Avatar />
-
+        <div className='box'>
+                <ul>
+                <Avatar />
+                    <a className='nome'>{props.user.nome}</a>
+                    <a className='profissao'>{props.user.profissao}</a>
+                </ul>
+        </div>
     );
 }
 
