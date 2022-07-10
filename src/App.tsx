02c9ced04a8dab322e685cb611eaf200f109e8ea
rick-ssa <rick_ssa@hotmail.com>
2022-07-10 00:24:06 -0300

    made component project activity

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,48 @@ import { Button } from '@mui/material';
 import React from 'react';
 import ItemProject from './components/ItemProject';
 import RestoreIcon from '@mui/icons-material/Restore'
+import ProjectActivity from './components/ProjectActivity';
+
+const button1 = <Button variant='contained' startIcon={<RestoreIcon />} color='error'>Restore </Button>
+const button2 = <Button variant='outlined'>view</Button>
+
+const myItems = [
+  {
+    tag: "upload",
+    time: "14:30",
+    title: "comprar comida",
+    author: "Ricardo Santos",
+    tagBgColor: "black",
+    tagColor: "white",
+    button: button1,
+  },
+  {
+    tag: "task",
+    time: "14:30",
+    title: "comprar roupa",
+    author: "Gabriel Moura",
+    tagBgColor: "red",
+    tagColor: "white",
+    button: button2,
+  },
+  {
+    tag: "buy",
+    time: "14:30",
+    title: "comprar carro",
+    author: "Keven",
+    tagBgColor: "yellow",
+    tagColor: "black",
+    button: button2,
+  }
+]
 
 function App() {
   return (
     <div>
-      <ItemProject 
-        tag='Upload' 
-        time="11:40 am" 
-        title="Comb - Home page.psd"
-        author='Ricardo Santos'
-        tagBgColor='red'
-        tagColor='white'
-      >
-        <Button variant='contained' startIcon={<RestoreIcon />} color='error'>Restore </Button>
-      </ItemProject>
-
-      <ItemProject 
-        tag='Task' 
-        time="13:25 am" 
-        title="Comb - Home page.psd"
-        author='Ricardo Santos'
-        tagBgColor='blue'
-        tagColor='white'
-      >
-        <Button variant='outlined'>view</Button>
-      </ItemProject>
+      <ProjectActivity 
+        items = {myItems}
+        title="PROJECT ACTIVITY"
+      />
     </div>
   );
 }
