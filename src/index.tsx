75c9de49859e33857c460f00b1178d1530f022bd
rick-ssa <rick_ssa@hotmail.com>
2022-07-10 22:02:18 -0300

    separando espaço de usuario dev

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Ricardo from './Ricardo'
+import Keven from './Keven'
+import Gabriel from './Gabriel'
 import reportWebVitals from './reportWebVitals';
+
+const DraftApp = () => {
+  const dev = require('./dev.json')
+  let app:any
+  switch(dev.user) {
+    
+    case "Ricardo":
+      app = <Ricardo />
+      break;
+    case "Keven":
+      app = <Keven />
+      break;
+    case "Gabriel":
+      app = <Gabriel />
+      break;
+  }
+
+  return app
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <DraftApp />
   </React.StrictMode>
 );
 
