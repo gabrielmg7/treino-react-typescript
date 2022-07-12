e77de9b8d3f2a0c2d0defd8a59e039aba4ce21e1
gabriel <gabrielmouraguimaraes@gmail.com>
2022-07-12 17:49:27 -0300

    using routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,79 @@
-import { Button } from '@mui/material';
-import React from 'react';
+import { Avatar, Button, Slider, TextField } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import ItemProject from './components/ItemProject';
+import RestoreIcon from '@mui/icons-material/Restore'
+import UserIcon from '@mui/icons-material/AccountCircle'
+import ProjectActivity from './components/ProjectActivity';
+import UserList from './components/userList';
+
+
+import api from './Api';
+
+
+const button1 = <Button variant='contained' startIcon={<RestoreIcon />} color='error'>Restore </Button>
+const button2 = <Button variant='outlined'>view</Button>
+const button3 = <Button variant='contained' startIcon={<UserIcon />} color='success'>User </Button>
+
+
+const getDadosFromApi = async (params: string) => {
+  const result = await api.get(params)
+
+  return result.data
+}
+
+function App() {
+  const [myItems, setMyItems] = useState<any>([])
+  const [results, setResults] = useState<number>(5)
+
+  useEffect(() => {
+    atualizarLista()
+
+  }, [results])
+
+  const atualizarLista = () => {
+    const items: any = []
+    console.log(myItems)
+    getDadosFromApi(`/?results=${results}`).then(response => {
+      response.results.map((item: any) => {
+        items.push({
+          key: item.email,
+          tag: item.location.country,
+          time: (new Date(item.registered.date).toLocaleDateString()),
+          title: `${item.name.first} ${item.name.last}`,
+          author: item.email,
+          tagBgColor: ["black", "blue", "red", "orange", "purple"][Math.floor(Math.random() * 5)],
+          tagColor: "white",
+          child: <Avatar alt={`${item.name.first} ${item.name.last}`} src={item.picture.medium} />,
+        })
+      })
+      setMyItems([...items])
+    })
+  }
+
+  return (
+
+    <div>
+      <UserList></UserList>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', padding: '20px' }}>
+        <Button variant='outlined' onClick={() => { atualizarLista() }}>
+          Atualizar List
+        </Button>
+
+        <Slider
+          aria-label="Default"
+          defaultValue={results}
+          valueLabelDisplay="auto"
+          min={1}
+          max={100}
+          onChangeCommitted={(e, v) => setResults(Number(v))}
+        />
+      </div>
+      <ProjectActivity
+        items={myItems}
+        title="PROJECT ACTIVITY"
+      />
+    </div>
+  );
+}
+
+export default App;
--- src/components/itemUser.tsx
+++ src/components/itemUser.tsx
@@ -1,76 +1,11 @@
-import Avatar from './avatar'
-import '../css/itemUser.css'
-import React from 'react'
-import api from '../Api';
 
-export interface UserItemProps {
-    results: [
-        {
-            gender: any,
-            name: {
-                title: any,
-                first: any,
-                last: any
-            },
-            location: {
-                street: {
-                    number: any,
-                    name: any,
-                },
-                city: any,
-                state: any,
-                country: any,
-                postcode: any,
-                coordinates: {
-                    latitude: any,
-                    longitude: any
-                },
-                timezone: {
-                    offset: any,
-                    description: any
-                }
-            },
-            email: any
-            login: {
-                uuid: any,
-                username: any,
-                password: any,
-                salt: any,
-                md5: any,
-                sha1: any,
-                sha256: any
-            },
-            dob: {
-                date: any,
-                age: any
-            },
-            registered: {
-                date: any,
-                age: any
-            },
-            phone: any,
-            cell: any,
-            id: {
-                name: any,
-                value: any
-            },
-            picture: {
-                large: any,
-                medium: any,
-                thumbnail: any
-            },
-            nat: any
-        }
-    ],
-    info: {
-        seed: any,
-        results: any,
-        page: any,
-        version: any
-    }
-}
+import '../css/itemUser.css';
+import React from 'react';
+import Avatar from './Avatar';
+import { UserItemProps } from './userList';
 
-export default function ItemUser(props: any) {
+
+const ItemUser = (props: any) => {
     const [state, setState] = React.useState<UserItemProps>({
         results: [
             {
@@ -138,23 +73,14 @@ export default function ItemUser(props: any) {
         }
     });
 
-    React.useLayoutEffect(() => {
-        api.get("/?results=10")
-            .then((response) => setState(response.data))
-            .catch((erro) => {
-                console.error("ops! ocorreu um erro" + erro);
-            });
-    }, []);
-
     return (
         <div className='user-item-container'>
             <Avatar />
             <div className='user-name-content'>
-                <div className='Nome'>{state.results[0].name.first + " " + state.results[0].name.last}
-                </div>
-                <div className='Nacionalidade'>{state.results[0].location.state}
-                </div>
+                <div className='Nome'>{state.results[0].name.first + " " + state.results[0].name.last}</div>
+                <div className='Nacionalidade'>{state.results[0].location.state}</div>
             </div>
         </div>
     );
 }
+export default ItemUser;
