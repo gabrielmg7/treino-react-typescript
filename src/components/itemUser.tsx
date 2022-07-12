794426e5a3522e07ccdced6788ad8ca0a9d9a96e
gabriel <gabrielmouraguimaraes@gmail.com>
2022-07-12 20:37:28 -0300

    .

--- src/components/itemUser.tsx
+++ src/components/itemUser.tsx
@@ -2,7 +2,7 @@
 import '../css/itemUser.css';
 import '../css/avatar.css';
 import React from 'react';
-import { UserItemProps } from './userList';
+
 
 const Photo = (props: any) => {
     return (
@@ -11,16 +11,18 @@ const Photo = (props: any) => {
         </div>
     );
 };
+
 const UserData = (props: any) => {
     return (
         <div className='user-name-content'>
             <div className='Nome'>{props.results[0].name.first + " " + props.results[0].name.last}</div>
             <div className='Nacionalidade'>{props.results[0].location.state}</div>
-
         </div>
     );
 };
+
 const ItemUser = (props: any) => {
+
     return (
         <div>
             <Photo />
