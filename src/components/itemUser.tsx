623e41e2209bb313275e62008a0ae78038c9c335
gabriel <gabrielmouraguimaraes@gmail.com>
2022-07-12 20:48:52 -0300

    .

--- src/components/itemUser.tsx
+++ src/components/itemUser.tsx
@@ -3,30 +3,16 @@ import '../css/itemUser.css';
 import '../css/avatar.css';
 import React from 'react';
 
-
-const Photo = (props: any) => {
-    return (
-        <div className='root'>
-            <div className='Avatar'>{props.results[0].picture.medium}</div>
-        </div>
-    );
-};
-
-const UserData = (props: any) => {
-    return (
-        <div className='user-name-content'>
-            <div className='Nome'>{props.results[0].name.first + " " + props.results[0].name.last}</div>
-            <div className='Nacionalidade'>{props.results[0].location.state}</div>
-        </div>
-    );
-};
-
 const ItemUser = (props: any) => {
-
     return (
         <div>
-            <Photo />
-            <UserData />
+            <div className='root'>
+                <img src={props.avatar} alt={"User Avatar"} />
+            </div>
+            <div className='user-name-content'>
+                <div className='Nome'>{props.firstname + " " + props.lastname}</div>
+                <div className='Nacionalidade'>{props.nacionalidade}</div>
+            </div>
         </div>
     );
 }
